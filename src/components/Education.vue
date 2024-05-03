4d91d1cfa56c80451c76256cb0e12531bb77fb89
borisovvilyaa <borisovvilyaa@gmail.com>
2024-05-03 15:10:41 +0300

    add new course

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -4,14 +4,14 @@
       <h3>Education</h3>
     </div>
     <div class="row">
-      <!-- First Project Section -->
+      <h2 style="margin-bottom: -20px; margin-top: 50px;" class="fw-bold">Higher and secondary education</h2>
+
       <div class="col-md-6 col-sm-12 mb-4">
         <div class="text-education-name">
           <h3>
             Software Engineer - Bachelor | National Technical University – «Kharkiv Polytechnic
             Institute», Ukraine | GPA: 4.2 | Sep 2021 - Jul 2025
           </h3>
-          <!-- Project name with a link and an image -->
           <p>
             The primary emphasis is on the hardware aspect of computers, which is a crucial
             component alongside software programming. The curriculum covers topics such as web
@@ -19,35 +19,21 @@
             places significant importance on cultivating game development skills, which is another
             integral aspect of the specialization.
           </p>
-          <!-- Key features of the project listed as bullet points -->
         </div>
         <div class="text-education-name">
           <h3>
             Computer since | Kharkiv gymnasium 83, Ukraine | GPA: 4.0 & Excellent CS certificate |
             Sep 2010 - Jun 2021
           </h3>
-          <!-- Project name with a link and an image -->
           <p>
             Specialty "Computer technology with advanced study". In 2020 he took the 3rd place at
             the city Olympiad in the field of game creation.
           </p>
-          <!-- Key features of the project listed as bullet points -->
         </div>
       </div>
-      <div class="col-md-6 col-sm-12 mb-4">
-        <div class="text-education-name">
-          <h3>
-            Cryptography and information security theory | Distributed Lab, Ukraine | GPA: 3.8 | May
-            2023 - Aug 2023
-          </h3>
-          <!-- Project name with a link and an image -->
-          <p>
-            During my cryptography course, I covered basic properties, cryptology definitions,
-            modular arithmetic, hash functions, symmetric encryption, classical ciphers, statistical
-            analysis, key exchange, Diffie-Hellman, RSA, digital signatures, elliptic curves, and
-            security issues in cryptosystems.
-          </p>
-        </div>
+      <div class="col-md-6 col-sm-12">
+        <h2 style="margin-bottom: -20px; margin-top: -25px;" class="fw-bold">Program language</h2>
+
         <div class="text-education-name">
           <h3>
             Golang programming on fundamental aspects | Distributed Lab, Ukraine | GPA: 5.0 | Apr
@@ -64,18 +50,18 @@
             programming principles
           </p>
         </div>
+        <h2 style="margin-bottom: -20px;" class="fw-bold">Cryptography and Math</h2>
         <div class="text-education-name">
           <h3>
-            Basic theory about decentralized finance | Distributed Lab, Ukraine | GPA: 4.0 | Feb
-            2023 - Apr 2023
+            Cryptography and information security theory | Distributed Lab, Ukraine | GPA: 3.8 | May
+            2023 - Aug 2023
           </h3>
           <!-- Project name with a link and an image -->
           <p>
-            The course covered stablecoins (USDT, USDC, DAI, FRAX), decentralized exchanges
-            (Uniswap, Uniswap v2, Uniswap v3, Curve), lending protocols (AAVE and Compound),
-            financial derivatives, GMX, Synthetix, zero-knowledge proof technology, decentralized
-            identity (Polygon ID). In addition, we discussed liquid steaking with Lido and Rocket
-            Pool.
+            During cryptography course, I covered basic properties, cryptology definitions,
+            modular arithmetic, hash functions, symmetric encryption, classical ciphers, statistical
+            analysis, key exchange, Diffie-Hellman, RSA, digital signatures, elliptic curves, and
+            security issues in cryptosystems.
           </p>
         </div>
         <div class="text-education-name">
@@ -93,6 +79,31 @@
             systems, participants' roles in the Bitcoin system, and the security model in
             decentralized accounting systems during course.
           </p>
+        </div>
+        <h2 style="margin-bottom: -20px;" class="fw-bold">DeFi</h2>
+        <div class="text-education-name">
+          <h3>
+            Basic theory about decentralized finance | Distributed Lab, Ukraine | GPA: 4.0 | Feb
+            2023 - Apr 2023
+          </h3>
+          <!-- Project name with a link and an image -->
+          <p>
+            The course covered stablecoins (USDT, USDC, DAI, FRAX), decentralized exchanges
+            (Uniswap, Uniswap v2, Uniswap v3, Curve), lending protocols (AAVE and Compound),
+            financial derivatives, GMX, Synthetix, zero-knowledge proof technology, decentralized
+            identity (Polygon ID). In addition, we discussed liquid steaking with Lido and Rocket
+            Pool.
+          </p>
+        </div>
+        <div class="text-education-name">
+          <h3>
+            General theory about decentralized finance | Distributed Lab, Ukraine | GPA:
+            4.0 | Jan 2024 - Apr 2024
+          </h3>
+          <p>
+            The curriculum included stablecoins such as USDT, USDC, DAI, and FRAX, decentralized exchanges like Uniswap, Uniswap v2, Uniswap v3, and Curve, lending protocols including AAVE and Compound, financial derivatives, GMX, Synthetix, zero-knowledge proof technology, and decentralized identity through Polygon ID. Furthermore, liquid staking with Lido and Rocket Pool was also explored.
+          </p>
+          
         </div>
         <span class="ps"
           >P.S: In all courses was tasks with coding.
